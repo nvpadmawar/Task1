436afb5876be2809ae480982ee382a802c1de6f2
nvpadmawar <nvpadmawar@gmail.com>
2021-05-27 12:19:54 +0530

    multiplication

---
+++ operation2.java
@@ -0,0 +1,17 @@
+class operation2{
+ pubblic static void main (string[] args){
+   
+  int a,b,c;
+   void mul(){
+    
+   System.out.println("Enter a value : "+a);
+   System.out.println("Enter b value : "+b);
+   c=a*b;
+   System.out.println(" value of c: "+c);
+
+
+    }
+
+  }
+
+}
