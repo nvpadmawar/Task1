7fb6d9edbc4687e27d587ff1ccedaf96c3b6991b
nvpadmawar <nvpadmawar@gmail.com>
2021-05-27 12:16:56 +0530

    subtraction

---
+++ operation1.java
@@ -0,0 +1,17 @@
+class operation1{
+ pubblic static void main (string[] args){
+   
+  int a,b,c;
+   void sub(){
+    
+   System.out.println("Enter a value : "+a);
+   System.out.println("Enter b value : "+b);
+   c=a-b;
+   System.out.println(" value of c: "+c);
+
+
+    }
+
+  }
+
+}
